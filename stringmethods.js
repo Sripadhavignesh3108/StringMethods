56b58f1ce753ac216a6e3b56eae7d030ceaf5f67
Sripadhavignesh3108 <sripadhavignesh@gmail.com>
2024-06-27 20:59:36 +0530

    string methods task

---
+++ stringmethods.js
@@ -0,0 +1,171 @@
+// PALINDROME TASK
+
+// let str="mom";
+// let stn="";
+// for(i=str.length-1;i>=0;i--)
+// {
+//     stn=stn+str[i]
+// }
+// if(str===stn){
+// console.log(`${str} It a Palindrome`)
+// }
+// else{
+//     console.log(`${str} its not a palindrome`)
+// }
+
+
+
+
+// CAPTIAL LETTER CONVERTER
+
+// let sam="hi good morning";
+// let stn=sam.charAt(0);
+// stm=stn.toUpperCase(1)+sam.slice(1);
+// console.log(stm)
+
+
+
+// COUNT A SPECIFIC LETTER IN WORD
+// let str="hellllo world";
+// let count="";
+// for(i=0;i<=str.length-1;i++)
+// {
+//  if(str[i]==="l")
+//  {
+//     count++;
+//  }
+// }
+// console.log(`count of 'l' is : ${count}`)
+
+
+
+// REVERSE THE WORD IN A SENTANCE
+// let stn="vinay veerapaneni";
+// let str="";
+// for(i=stn.length-1;i>=0;i--)
+// {
+//     str+=stn[i]
+// }
+// console.log(`Given Sentance is : ${stn}`)
+
+// console.log(`reverse of given Sentance is : ${str}`)
+
+
+
+
+// COUNTING NO.OF CHARECTERS IN A STRING.
+// let str="vignesh sripadha";
+// let count="";
+// for(i=0;i<=str.length-1;i++)
+//  { 
+//      count++;
+// }
+// console.log(`No.of Charecters in a string : ${count}`)
+
+
+
+
+// CONVERT TO UPPERCASE
+// let str="vignesh sripadha";
+// let stn=str.toUpperCase();
+// console.log(stn)
+
+
+
+
+// INDEX OF SUBSTRING
+// let str="vignesh sripadha";
+// let stn=str.substring(8,11)
+// console.log(stn[1])
+
+
+
+// REPLACE A SUBSTRING WITH ANOTHER STRING
+// let str="hello world";
+// let stn=str.substring(0,5)
+// let sta=stn.replace("hello","hi")
+// console.log(sta)
+
+
+
+
+// TRIM A WHITE SPACE FROM A STRING
+// let str="    vignesh sripadha      ";
+// let stn=str.trim();
+// console.log(stn)
+
+
+
+
+//SPLITING A STRING
+// let str="hello";
+// let stn=str.split("")
+// console.log(stn)
+
+
+
+
+// CONCATATION OF TWO STRINGS
+// let str="";
+// let stn=str.concat("HI ","vignesh")
+// console.log(stn)
+
+
+
+
+//Check if a String Starts with a Substring:
+
+// let str="hello world hello";
+// let stn=str.substring(12,17)
+// let sta=str.slice(0,5)
+// if(sta===stn){
+//     console.log("String Start's with Substring")
+// }
+// else
+// {
+//     console.log(`String NOT Start's with Substring`)
+// }
+
+
+
+
+// Check if a String Ends with a Substring:
+
+// let str="hello world hallo";
+// let stn=str.substring(0,5)
+// let sta=str.slice(-5)
+// if(sta===stn){
+//     console.log("String Start's with Substring")
+// }
+// else
+// {
+//     console.log(`String NOT Start's with Substring`)
+// }
+
+
+
+
+// Get a Substring:
+// let str="vignesh sripadha";
+// let stn=str.substring(8)
+// console.log(stn)
+
+
+
+
+// Reverse a String:
+// let str="vignesh sripadha";
+// let stn=str.substring(8)
+// let sta="";
+// for(i=stn.length-1;i>=0;i--)
+// {
+//     sta+=stn[i]
+// }
+// console.log(sta)
+
+
+
+// Convert String to Lowercase.
+// let str="VIGNESH SRIPADHA";
+// let sta=str.toLowerCase()
+// console.log(sta)
